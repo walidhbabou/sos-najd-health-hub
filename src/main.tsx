a760b043bbbdae9bcc9b7776aebfbaaca370630c
walidhbabou <161231266+walidhbabou@users.noreply.github.com>
2026-05-04 09:04:44 +0100

    ff

---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "@tanstack/react-router";
+
+import { getRouter } from "./router";
+
+const router = getRouter();
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+  throw new Error("Root element #root not found");
+}
+
+ReactDOM.createRoot(rootElement).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+);
